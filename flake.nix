7cd7bdda81d13b63a010ea92487c51d65cbad4a0
Seigo Mori <85787242+s3igo@users.noreply.github.com>
2024-07-08 00:51:40 +0900

    Test/deploy (#1)
    * feat: add config to build image
    
    * feat: add deploy workflow
    
    * fix: add pkg-config for linux
    
    * fix: pkg-config
    
    * chore: remove rustfmt from the toolchain
    
    * fix: add libssl-dev
    
    * fix: build error
    
    * fix: build on amd64 ubuntu
    
    * fix: build failure
    
    * feat(ci): add cache
    
    * fix: change to not use openssl
    
    * feat: disable auto stop
    
    * chore: remove unnecessary branch

--- flake.nix
+++ flake.nix
@@ -8,12 +8,9 @@
     };
     crane = {
       url = "github:ipetkov/crane";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim.url = "github:s3igo/dotfiles?dir=neovim";
+    neovim-builder.url = "github:s3igo/dotfiles?dir=neovim";
   };
 
   outputs =
@@ -23,55 +20,55 @@
       flake-utils,
       fenix,
       crane,
-      neovim,
+      neovim-builder,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        toolchain =
-          with fenix.packages.${system};
-          combine [
-            (fromToolchainFile {
-              file = ./rust-toolchain.toml;
-              sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
-            })
-            default.rustfmt # rustfmt nightly
-          ];
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+        };
         craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
         src = craneLib.cleanCargoSource ./.;
         buildInputs =
           with pkgs;
-          lib.optional stdenv.isDarwin [
+          lib.optionals stdenv.isDarwin [
             libiconv
             darwin.apple_sdk.frameworks.SystemConfiguration
           ];
         commonArgs = {
           inherit src buildInputs;
           strictDeps = true;
-          # CARGO_TARGET_AARCH64_APPLE_DARWIN_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.lld}/bin/ld64.lld";
         };
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
       in
       {
-        packages = {
-          neovim = neovim.withModules {
+        packages = rec {
+          neovim = neovim-builder.withModules {
             inherit system pkgs;
-            modules = with neovim.modules; [
+            modules = with neovim-builder.modules; [
               im-select
               nix
               rust
             ];
           };
-
           default = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+          container = pkgs.dockerTools.buildImage rec {
+            name = "btw";
+            tag = "latest";
+            copyToRoot = [ default ];
+            config.Cmd = [ "/bin/${name}" ];
+          };
         };
 
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = buildInputs;
-          buildInputs =
-            [
+          packages =
+            buildInputs
+            ++ [
               toolchain
+              fenix.packages.${system}.default.rustfmt # rustfmt nightly
               self.packages.${system}.neovim
             ]
             ++ (with pkgs; [
