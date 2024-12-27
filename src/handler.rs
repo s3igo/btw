d64a7c9079153a59dcbb8514dd642e7a3d981eee
s3igo <85787242+s3igo@users.noreply.github.com>
2024-12-28 02:40:34 +0900

    fix: Update URL detection strategy
    Switch from `message` to `message_update` handler for link unfurling.

--- src/handler.rs
+++ src/handler.rs
@@ -27,19 +27,38 @@ impl Handler {
     }
 }
 
+async fn get_message(
+    ctx: &serenity::client::Context,
+    channel_id: &serenity::model::id::ChannelId,
+    message_id: &serenity::model::id::MessageId,
+) -> anyhow::Result<serenity::model::channel::Message> {
+    let msg = channel_id.message(&ctx.http, message_id).await?;
+    Ok(msg)
+}
+
 #[serenity::async_trait]
 impl serenity::client::EventHandler for Handler {
-    async fn message(
+    /// This handler listens for `message_update` events rather than `message`
+    /// events to ensure link unfurling has completed.
+    /// This guarantees that `msg.embeds` contains the unfurled content.
+    /// For more about unfurling, see: https://api.slack.com/reference/messaging/link-unfurling
+    /// NOTE: Since Discord's documentation does not contain information about
+    /// unfurling, a link to Slack's explanation is provided instead.
+    async fn message_update(
         &self,
         ctx: serenity::client::Context,
-        msg: serenity::model::channel::Message,
+        _old: Option<serenity::model::channel::Message>,
+        _new: Option<serenity::model::channel::Message>,
+        event: serenity::model::event::MessageUpdateEvent,
     ) {
-        if let Err(e) = self.inner(ctx, msg).await {
-            eprintln!("Error: {e:?}");
+        if let Ok(msg) = get_message(&ctx, &event.channel_id, &event.id).await {
+            if let Err(e) = self.inner(ctx, msg).await {
+                eprintln!("Error: {e:?}");
+            }
         }
     }
 
-    async fn ready(&self, _: serenity::client::Context, ready: serenity::model::gateway::Ready) {
+    async fn ready(&self, _ctx: serenity::client::Context, ready: serenity::model::gateway::Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
